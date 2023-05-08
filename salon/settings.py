d51edb272028a77e85027c608dd00d6c3ba92d13
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2023-05-08 07:42:25 +0100

    updated

--- salon/settings.py
+++ salon/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -93,9 +93,11 @@ WSGI_APPLICATION = 'salon.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
 }
+
 
 
 # Password validation
