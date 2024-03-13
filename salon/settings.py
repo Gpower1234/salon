aea617fe31b51b5349b9689372ab9421068b39a7
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-03-13 11:19:06 +0100

    updated

--- salon/settings.py
+++ salon/settings.py
@@ -93,20 +93,19 @@ WSGI_APPLICATION = 'salon.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-
+'''
 DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
 }
-
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 
 # Password validation
